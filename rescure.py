c69f1295a13764ee4e73c60822ad2672bcdb907d
jkantorow <jkantorow@gmail.com>
2023-12-13 23:20:16 -0500

    switching back

--- rescure.py
+++ rescure.py
@@ -329,7 +329,7 @@ def Hrxn_pp(n_molphen):
     Hrxn_pp = Hrxn_total_100p/Na # J/ph_mol
     n_phenol = n_molphen * Na
 
-    return Hrxn_pp * n_phenol
+    return Hrxn_pp * n_phenol * 10**-6
 
 # Here is the function for a phenol - coal reaction:
 
@@ -340,7 +340,7 @@ def Hrxn_pc(n_molcoal):
     Hrxn_pc = Hrxn_total_60p/Na # J/coal_mol
     n_coal = n_molcoal * Na
 
-    return Hrxn_pc * n_coal
+    return Hrxn_pc * n_coal * 10**-6
 
 # Here is the function to calculate the system's new
 # temperature after each iteration based on the heat
@@ -358,7 +358,7 @@ def temp_vs_hrxn(Hrxn, n_mol):
         k_all = 0.24549951294321715
         x0_all = 153.05731157934324
 
-        return ((np.log(L_all/(Hrxn*600*n_mol) - 1)/(-k_all)) + x0_all) * 0.1
+        return ((np.log(L_all/(Hrxn) - 1)/(-k_all)) + x0_all) * 0.1
 
 # Here is a function that calculates the new 
 # state of the system:
